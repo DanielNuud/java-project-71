da171dc5ba3d74c3c7f004be576d056c376b7a8f
DanielNuud <daaniel.nuud@gmail.com>
2023-10-30 09:35:39 +0200

    adding CommandLine functions

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,30 @@
 package hexlet.code;
 import hexlet.code.Differ;
+import picocli.CommandLine;
 
-public class App {
+import java.io.File;
+
+@CommandLine.Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable{
+    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean helpRequested;
+
+    @CommandLine.Option(names = { "-V", "--version" }, versionHelp = true, description = "Print version information and exit.")
+    private boolean versionRequested;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine cmd = new CommandLine(new App());
+        cmd.execute(args);
     }
 
+    @Override
+    public void run() {
+        if (helpRequested) {
+            CommandLine.usage(this, System.out);
+        } else if (versionRequested) {
+            System.out.println("Version 1.0");
+        } else {
+            System.out.println("Running the diff operation...");
+        }
+    }
 }

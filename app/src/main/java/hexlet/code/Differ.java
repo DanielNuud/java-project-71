ee538853c882f389706f5fc52e904b4053262b5e
DanielNuud <daaniel.nuud@gmail.com>
2023-11-03 15:08:55 +0200

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,9 +1,11 @@
 package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+
 public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
         Map<String, Object> data1 = readJSONFile(filePath1);
@@ -16,20 +18,31 @@ public class Differ {
 
         StringBuilder result = new StringBuilder("{\n");
 
-        for (String key : keys) {
-            Object value1 = data1.get(key);
-            Object value2 = data2.get(key);
+        Set<String> processedKeys = new HashSet<>();
 
-            if (value1 == null) {
-                result.append("  + ").append(key).append(": ").append(value2).append("\n");
-            } else if (value2 == null) {
-                result.append("  - ").append(key).append(": ").append(value1).append("\n");
-            } else if (!value1.equals(value2)) {
-                result.append("  - ").append(key).append(": ").append(value1).append("\n");
-                result.append("  + ").append(key).append(": ").append(value2).append("\n");
+        for (String key : keys) {
+            if (!processedKeys.contains(key)) {
+                boolean containsKey1 = data1.containsKey(key);
+                boolean containsKey2 = data2.containsKey(key);
+
+                if (containsKey1 && !containsKey2) {
+                    result.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
+                    processedKeys.add(key);
+                } else if (!containsKey1 && containsKey2) {
+                    result.append("  + ").append(key).append(": ").append(data2.get(key)).append("\n");
+                    processedKeys.add(key);
+                } else if (containsKey1 && containsKey2) {
+                    if (!data1.get(key).equals(data2.get(key))) {
+                        result.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
+                        result.append("  + ").append(key).append(": ").append(data2.get(key)).append("\n");
+                        processedKeys.add(key);
+                    } else {
+                        result.append("    ").append(key).append(": ").append(data1.get(key)).append("\n");
+                        processedKeys.add(key);
+                    }
+                }
             }
         }
-
         result.append("}");
         return result.toString();
     }

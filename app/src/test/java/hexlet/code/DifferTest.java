4de631128ae5cb47777f622d2499ab92f5230e04
DanielNuud <daaniel.nuud@gmail.com>
2023-11-18 12:50:17 +0200

    Update DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -8,8 +8,8 @@ class DifferTest {
     @Test
     void testGenerateWhenFilesHaveDifferences() throws Exception {
         String currentDirectory = System.getProperty("user.dir");
-        String filePath1 = currentDirectory + "/file1.json";
-        String filePath2 = currentDirectory + "/file2.json";
+        String filePath1 = currentDirectory + "/src/test/resources/file1.json";
+        String filePath2 = currentDirectory + "/src/test/resources/file2.json";
 
         String result = Differ.generate(filePath1, filePath2);
 

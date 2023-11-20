2c581404be3ff4e709f173d28bdab830f511cbca
DanielNuud <daaniel.nuud@gmail.com>
2023-11-20 15:20:25 +0200

    add one more test

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,6 +1,7 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
@@ -13,15 +14,31 @@ class DifferTest {
 
         String result = Differ.generate(filePath1, filePath2);
 
-        String expected = """
-                {
-                  - follow: false
-                    host: hexlet.io
-                  - proxy: 123.234.53.22
-                  - timeout: 50
-                  + timeout: 20
-                  + verbose: true
-                }""";
+        String expected = "{" +
+                "\n  - follow: false" +
+                "\n    host: hexlet.io" +
+                "\n  - proxy: 123.234.53.22" +
+                "\n  - timeout: 50" +
+                "\n  + timeout: 20" +
+                "\n  + verbose: true" +
+                "\n}";
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testGenerateWhenFilesAreSimilar() throws Exception {
+        String currentDirectory = System.getProperty("user.dir");
+        String filePath1 = currentDirectory + "/src/test/resources/file1.yaml";
+        String filePath2 = currentDirectory + "/src/test/resources/file2.yaml";
+
+        String result = Differ.generate(filePath1, filePath2);
+
+        String expected = "{" +
+                "\n    follow: false" +
+                "\n    host: hexlet.io" +
+                "\n    proxy: 123.234.53.22" +
+                "\n    timeout: 50" +
+                "\n}";
         assertEquals(expected, result);
     }
 

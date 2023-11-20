3f04b1e81b37132a0b853c17d794a0f38ddaabd3
DanielNuud <daaniel.nuud@gmail.com>
2023-11-20 15:20:58 +0200

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,12 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -14,10 +7,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
+import static hexlet.code.Parser.readFileBySpecialty;
+
 public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
-        Map<String, Object> data1 = readJSONFile(filePath1);
-        Map<String, Object> data2 = readJSONFile(filePath2);
+        Map<String, Object> data1 = readFileBySpecialty(filePath1);
+        Map<String, Object> data2 = readFileBySpecialty(filePath2);
 
         List<String> keys = new ArrayList<>();
         keys.addAll(data1.keySet());
@@ -54,14 +49,4 @@ public class Differ {
         result.append("}");
         return result.toString();
     }
-
-    public static Map<String, Object> readJSONFile(String filePath) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File(filePath);
-        String absolutePath = file.getAbsolutePath();
-        String fileContent = new String(Files.readAllBytes(Paths.get(absolutePath)));
-        return objectMapper.readValue(fileContent, new TypeReference<Map<String, Object>>() {
-        });
-    }
-
 }

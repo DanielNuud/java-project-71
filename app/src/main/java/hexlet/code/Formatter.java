4d166d1c048f7fe52b86c2564e574a1548ec193e
DanielNuud <daaniel.nuud@gmail.com>
2023-11-23 15:50:35 +0200

    add Formatter

---
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+
+import formatters.Json;
+import formatters.Stylish;
+
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Formatter {
+    public static String makeFormat(TreeMap<String, Object> value1, TreeMap<String, Object> value2,
+                                    String format, TreeSet<String> setKeys) throws Exception {
+
+        return switch (format) {
+            case "json" -> Json.format(value1, value2, setKeys);
+            case "stylish" -> Stylish.format(value1, value2, setKeys);
+            default -> throw new Exception("Unknown output format!");
+        };
+    }
+}

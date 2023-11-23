86a19b74cf275f4ad9112b9ced98f7f9e05bc0e0
DanielNuud <daaniel.nuud@gmail.com>
2023-11-23 16:02:54 +0200

    fixing linter errors

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 
 import static hexlet.code.Parser.readFileBySpeciality;
 
--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -4,16 +4,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class Parser {
-    public static TreeMap<String, Object> readFileBySpeciality(String absoluteFilePath, String filePath) throws Exception {
+    public static TreeMap<String, Object> readFileBySpeciality(String absoluteFilePath, String filePath)
+            throws Exception {
         if (filePath.endsWith(".json")) {
             return readJSONFile(absoluteFilePath);
         } else if (filePath.endsWith(".yaml")) {
@@ -25,14 +21,14 @@ public class Parser {
     public static TreeMap<String, Object> readJSONFile(String filePath) throws IOException {
         final ObjectMapper objectMapper = new ObjectMapper();
         final TreeMap<String, Object> value;
-        value = objectMapper.readValue(filePath, new TypeReference<>() {});
+        value = objectMapper.readValue(filePath, new TypeReference<>() { });
         return value;
     }
 
     public static TreeMap<String, Object> readYAMLFile(String filePath) throws Exception {
         final ObjectMapper objectMapper = new YAMLMapper();
         final TreeMap<String, Object> value;
-        value = objectMapper.readValue(filePath, new TypeReference<>() {});
+        value = objectMapper.readValue(filePath, new TypeReference<>() { });
         return value;
     }
 }
